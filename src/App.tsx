8b641bc17d713f25c9b496737930fa90a14e27c8
tooooo1 <wer4272@nate.com>
2022-11-23 03:37:56 +0900

    feat: footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Program from '@/components/Program.jsx';
 import Speakers from '@/components/Speaker/Speakers.jsx';
 import styled from '@emotion/styled';
+import Footer from './components/Footer.js';
 
 const App = () => {
   return (
     <Wrapper>
       <Program />
       <Speakers />
+      <Footer />
     </Wrapper>
   );
 };
---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+const Footer = () => {
+  return <Wrapper>F.univ</Wrapper>;
+};
+
+export default Footer;
+
+const Wrapper = styled.div`
+  padding: 5rem 0 3rem 0;
+  text-align: center;
+  opacity: 0.8;
+`;
