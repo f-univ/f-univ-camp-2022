306933aa528a23aaf40476b999a3096ffa2a61b6
tooooo1 <wer4272@nate.com>
2022-11-23 02:41:27 +0900

    rename: 폴더 변경

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -6,7 +6,7 @@ export const speakers = [
     subTitle: '주최자',
     sessionNumber: 1,
     time: '13:00 - 13:15',
-    imageUrl: '/public/tooooo1_speaker.png',
+    imageUrl: '/public/images/tooooo1_speaker.png',
   },
   {
     name: '스피커',
@@ -15,7 +15,7 @@ export const speakers = [
     subTitle: '타이틀',
     sessionNumber: 2,
     time: '13:15 - 13:30',
-    imageUrl: '/public/speaker.png',
+    imageUrl: '/public/images/speaker.png',
   },
   {
     name: '스피커',
@@ -24,7 +24,7 @@ export const speakers = [
     subTitle: '타이틀',
     sessionNumber: 3,
     time: '13:30 - 13:45',
-    imageUrl: '/public/speaker.png',
+    imageUrl: '/public/images/speaker.png',
   },
   {
     name: '스피커',
@@ -33,7 +33,7 @@ export const speakers = [
     subTitle: '타이틀',
     sessionNumber: 4,
     time: '13:45 - 14:00',
-    imageUrl: '/public/speaker.png',
+    imageUrl: '/public/images/speaker.png',
   },
   {
     name: '스피커',
@@ -42,7 +42,7 @@ export const speakers = [
     subTitle: '타이틀',
     sessionNumber: 5,
     time: '14:00 - 14:15',
-    imageUrl: '/public/speaker.png',
+    imageUrl: '/public/images/speaker.png',
   },
   {
     name: '스피커',
@@ -51,6 +51,6 @@ export const speakers = [
     subTitle: '타이틀',
     sessionNumber: 6,
     time: '14:15 - 14:30',
-    imageUrl: '/public/speaker.png',
+    imageUrl: '/public/images/speaker.png',
   },
 ];
