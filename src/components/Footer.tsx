b6b7904c62d9c1e82db652ec2dee659df9b394f5
tooooo1 <wer4272@nate.com>
2022-11-23 15:52:58 +0900

    feat: GitHub link, designer shoutout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,15 @@
 import styled from '@emotion/styled';
 
 const Footer = () => {
-  return <Wrapper>F.univ</Wrapper>;
+  return (
+    <Wrapper>
+      <GitHub href="https://github.com/f-univ/f-univ-conf-2022" target="_blank">
+        GitHub
+      </GitHub>
+      <Copy>COPYRIGHT 2022 FE UNIV CONF | 2022</Copy>
+      <Designer>designed by CHEMIN</Designer>
+    </Wrapper>
+  );
 };
 
 export default Footer;
@@ -9,5 +17,25 @@ export default Footer;
 const Wrapper = styled.footer`
   padding: 5rem 0 3rem 0;
   text-align: center;
+  font-size: 0.8rem;
+  line-height: 2rem;
+`;
+
+const GitHub = styled.a`
+  color: white;
+  text-decoration: underline;
+  font-size: 0.9rem;
+  cursor: pointer;
+`;
+
+const Copy = styled.div`
   opacity: 0.8;
+  color: #bdbdbd;
+`;
+
+const Designer = styled.div`
+  font-size: 0.7rem;
+  opacity: 0.7;
+  color: #bdbdbd;
+  line-height: 0.5rem;
 `;
