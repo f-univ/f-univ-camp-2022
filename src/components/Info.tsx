c1a1c1ec9c8e9e86c44b13aa973fbabd672d6811
tooooo1 <wer4272@nate.com>
2022-11-26 23:58:34 +0900

    feat: 소개글 추가

---
+++ src/components/Info.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+const Info = () => {
+  return <Wrapper></Wrapper>;
+};
+
+export default Info;
+
+const Wrapper = styled.article`
+  width: 85%;
+  padding: 5rem 0 3rem 0;
+  margin: 0 auto;
+`;
