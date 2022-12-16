c5c813d318a0188beae514f70c23cd7220fc0855
tooooo1 <wer4272@nate.com>
2022-12-16 16:52:07 +0900

    refactor: stroke-width -> strokeWidth

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -41,7 +41,7 @@ const Info = () => {
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <line y1="1.75" x2="86" y2="1.75" stroke="white" stroke-width="2.5" />
+          <line y1="1.75" x2="86" y2="1.75" stroke="white" strokeWidth="2.5" />
         </svg>
       </Title>
       <ProgramDescription>
